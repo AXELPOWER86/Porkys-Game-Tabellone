1cacc6f61776f5f48624a7b137e1978ebafaf03f
AXELPOWER86 <potenzaalessandro@live.it>
2026-01-10 21:36:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,37 @@
-const CACHE_NAME = "porkys-game-v1";
+const CACHE_NAME = "porkys-game-v2";
+
 const FILES = [
   "./",
-  "./Porky's game - Carte.html",
+  "./index.html",
   "./manifest.json",
+  "./sw.js",
+
   "./icon-180.png",
-  "./Nuovo_logo_carte.png"
+  "./icon-192.png",
+  "./icon-512.png",
+
+  "./Nuovo_logo_carte.png",
+  "./Nuovo_logo_soft.png",
+  "./Logo_per_chinhot.png",
+
+  "./Tabellone_soft.html",
+  "./Tabellone_chinhot.html",
+  "./Tabellone_extreme.html"
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
   );
 });
