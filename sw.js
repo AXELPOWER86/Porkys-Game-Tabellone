a25d6d698742a2766b02d5293b24d5787a832a50
AXELPOWER86 <potenzaalessandro@live.it>
2026-01-15 20:16:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,20 @@
-const CACHE_NAME = 'porkys-game-v1';
-
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = "porkys-game-v1";
+const FILES = [
+  "./",
+  "./Porky's game - Carte.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./Nuovo_logo_carte.png"
 ];
 
-// Install
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
